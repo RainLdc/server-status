9fad319d12ff819192651b67ea20c75d439e51aa
Romain.leduc <romain.leduc@outlook.fr>
2020-08-26 14:04:57 +0200

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  transpileDependencies: ["vuetify"],
+  pwa: {
+    name: "Server Status",
+    themeColor: "#000000",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+  },
+};
