3d23daae9883f591ffc63628c7eff82586392b18
Romain.leduc <romain.leduc@outlook.fr>
2020-08-26 13:05:24 +0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>Etat de servers</h1>
+    <table class="table table-stripped table-hover" id="cont-table">
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Name</th>
+          <th>URL</th>
+          <th>Port</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(site, index) in sites" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ site.name }}</td>
+          <td>
+            <a v-bind:href="getHref(site)" target="_blank">{{ site.url }}</a>
+          </td>
+          <td>{{ site.port }}</td>
+          <td v-if="site.status">
+            <span class="label label-success">LIVE</span>
+          </td>
+          <td v-else>
+            <span class="label label-danger">DOWN</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import json from "../assets/data.json";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "HelloWorld",
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  data() {
+    return {
+      siteDataUrl: json,
+      sites: [],
+    };
+  },
+  created: function () {
+    this.loadData();
+  },
+
+  methods: {
+    getHref: function (site) {
+      if (!site.port) site.port = 80;
+      return `https://${site.url}:${site.port}`;
+    },
+
+    loadData: function () {
+      let self = this;
+      self.sites = json;
+      self.getStatus();
+    },
+
+    getStatus: function () {
+      let self = this;
+      self.sites.forEach(function (site, index) {
+        let url = `https://${site.url}`;
+        if (site.port && site.port != 80 && site.port != 443)
+          url += `:${site.port}`;
+
+        fetch(url, { mode: "no-cors" }).then((resp) => {
+          site.status = true;
+          if (!resp.ok || resp.status != 200) site.status = false;
+          if (resp.type == "opaque") site.status = true;
+          self.$set(self.sites, index, site);
+        });
+      });
+    },
+  },
+};
+</script>
